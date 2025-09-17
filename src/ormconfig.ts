8137597515dea6d2667c936ee9a07648684d212b
Fabiana Carvalho <fabiana.acarvalho@outlook.com>
2025-09-17 15:43:11 -0300

    Initial commit

---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'devuser',
+  password: '1234',
+  database: 'blog',
+  entities: [__dirname + '/**/*.entity.{.ts, .js}'],
+  synchronize: true, // don't use on production
+};
+
+export default config;
